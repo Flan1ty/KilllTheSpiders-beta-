99fbe453689e4c365dcc69da497a209c77e889a9
Flanity <holholggbv@gmail.com>
2023-06-21 23:48:30 +0300

    Добавьте файлы проекта.

---
+++ KilllTheSpiders/Programmers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KilllTheSpiders
+{
+    public partial class Programmers : Form
+    {
+        public Programmers()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
